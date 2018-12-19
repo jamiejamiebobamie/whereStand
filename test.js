0b9453df724adb0a46a6db73ce443acb24a74985
jamiejamiebobamie <jmccrory@vt.edu>
2018-12-18 20:21:34 -0500

    solved the Josephus problem using linkedlists in test.js

--- test.js
+++ test.js
@@ -1,3 +1,23 @@
+function winner(num){
+    // Calculating the 'winner' of the Josephus Problem.
+    // Returns the number of people playing, the winner, the winner's number in binary.
+    //The number of people playing gets progressively larger.
+    let range = {
+        7: [4,5,6,7,9],
+        6: [10,11,12,13,14],
+        5: [15,16,17,18,19],
+        4: [20,21,22,23,24],
+        3: [25,26,27,28,29],
+        2: [28,29,30,31,32,33],
+        1: [34,35,36,37,38,39,40],
+    }
+    let n = range[num][Math.floor(Math.random() * range[num].length)];
+    let b = convertToBinary(n);
+    let p = findHighestPow(b);
+    let winner = ((2*(n - p))+1);
+    return ([n, winner, truncateBinary(convertToBinary(winner))]);
+}
+
 function convertToBinary(num){
     //Takes in an int and returns the number in binary of base 2.
     let number = num
@@ -153,58 +173,76 @@ function truncateBinary(num){
     }
 }
 
-function makeArrNums(num){
-var range = new Array(num);
-for (var i = 0; i < range.length; i++){
-    range[i] = i+1;
+
+// while(alive > 1){
+//     // console.log(alive);
+//     alive = alive -1;
+// }
+// console.log(alive)
+
+
+class Node {
+    constructor(value, next, last){
+        this.value = value;
+        this.next = null;
+        this.last = null;
     }
-    return range;
 }
 
-function winner(num){
-    // Calculating the 'winner' of the Josephus Problem.
-    // Returns the number of people playing, the winner, the winner's number in binary.
-    //The number of people playing gets progressively larger.
-    let range = {
-        7: [4,5,6,7,9],
-        6: [10,11,12,13,14],
-        5: [15,16,17,18,19],
-        4: [20,21,22,23,24],
-        3: [25,26,27,28,29],
-        2: [28,29,30,31,32,33],
-        1: [34,35,36,37,38,39,40],
+class LinkedList { //doesn't work with lasts, but nexts work
+    constructor(){
+    this.head = null;
+    this.tail = null;
+    this.len = 0;
+    this.last = null
     }
-    let n = range[num][Math.floor(Math.random() * range[num].length)];
-    let b = convertToBinary(n);
-    let p = findHighestPow(b);
-    let winner = ((2*(n - p))+1);
-    return ([n, winner, truncateBinary(convertToBinary(winner))]);
+
+    addNode(value){
+        let newNode = new Node(value);
+        newNode.next = this.head;
+        if (this.last != null){
+            newNode.last = this.last
+            this.tail = newNode;
+            this.last.next = newNode
+        } else {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        this.len = this.len + 1;
+        this.last = newNode;
+    }
+
+    removeNode(node1, node2){ //remove node2
+        if (node2 != null){
+        node1.next = node2.next
+        this.len = this.len - 1; //I am not updating lasts
+    }
+}
 }
 
-var buttonS = document.getElementById('changeS');
-var peopleS = document.getElementById('peopleS');
-var winS = document.getElementById('winnerS');
-var binaryS = document.getElementById('binaryS');
-let win_jsS;
-var count = 7;
+var ll = new LinkedList();
 
-win_jsS = winner(count);
-peopleS.innerHTML = 3;
-winS.innerHTML = 3;
-binaryS.innerHTML = 11;
+var p = Math.floor(Math.random() * 6 + 1)
+var win = winner(p);
+console.log(win);
 
-buttonS.addEventListener("click", function(){
-    if (count > 0){
-    win_jsS = winner(count);
-    peopleS.innerHTML = win_jsS[0];
-    winS.innerHTML = win_jsS[1];
-    binaryS.innerHTML = win_jsS[2];
-    count = count - 1;
-    buttonS.innerHTML = count;
-} else if (count > -1){
-    peopleS.innerHTML = '41';
-    winS.innerHTML = '19';
-    binaryS.innerHTML = '10011';
-    count = 7;
+var alive = win[0]
+var living = []
+
+for (let i = 1; i <= alive; i++){
+    ll.addNode(i)
 }
-});
+
+
+var current = ll.head;
+
+// console.log(current.next)
+
+while (ll.len > 1) {
+    // console.log(ll.len)
+    ll.removeNode(current, current.next)
+    // console.log(current.value)
+    current = current.next
+}
+console.log(current.next.value)
+// console.log(alive)
