b0f89d7f44984e3351166b89034b0dae67829ec3
jamiejamiebobamie <jmccrory@vt.edu>
2018-12-25 18:25:21 -0500

    implemented chosen boolean structure

--- sketch/sprite.js
+++ sketch/sprite.js
@@ -10,28 +10,77 @@ class Sprite {
         this.x = x;
         this.y = y;
         this.n = n +1;
+        this.idle = true;
+        this.wave = false;
+        this.chosen = false;
+        this.freeze = 0;
+        this.speedStored = this.speed;
     }
 
-    show(){
-        // rotate(this.rotation);
-        if (dist(this.x+175, this.y+130, mouseX, mouseY) > 100) {
-        let z = 65;
-        let index = floor(this.index) % this.len_idle
-        image(this.animation_idle[index],this.x,this.y);
+
+
+playIdle(){
+    let z = 65;
+    let index = floor(this.index) % this.len_idle
+    image(this.animation_idle[index],this.x,this.y);
+    fill('#39FF14');
+    text(this.n,this.x+175,this.y+z)
+}
+
+playWave(){
+    let z = 65;
+    let index = floor(this.index) % this.len_wave
+    if (this.wave == true && index < 11){
+        image(this.animation_wave[index],this.x,this.y); //11
         fill('#39FF14');
         text(this.n,this.x+175,this.y+z)
-        console.log(dist(this.x+175, this.y+z, mouseX, mouseY))
+        if (index == 10){
+            this.speed = this.freeze
+            image(this.animation_wave[10],this.x,this.y); //11
+        }
+        if (this.chosen){
+            this.speed = this.speedStored
+            image(this.animation_wave[index],this.x,this.y); //11
+            if (index == this.len_wave-1){
+                playChosen();
+            }
+        }
+    }
+    if (this.wave == false){
+            this.speed = this.speedStored
+            image(this.animation_wave[index],this.x,this.y); //11
+        if (index == this.len_wave-1){
+            this.idle = true
+        }
+    }
+}
+
+playChosen(){
+    console.log(this.chosen)
+}
+
+show(){
+    // if (this.chosen == true){
+    //     this.playChosen();
+    // }
+    if (dist(this.x+175, this.y+130, mouseX, mouseY) > 100) {
+        this.wave = false
     } else {
-        let z = 65;
-        let index = floor(this.index) % this.len_wave
-        image(this.animation_wave[index],this.x,this.y);
-        fill('#39FF14');
-        text(this.n,this.x+175,this.y+z)
-        console.log(dist(this.x+175, this.y+z, mouseX, mouseY))
+        this.idle = false;
+        this.wave = true
     }
+    if(this.wave == true && mouseIsPressed){
+        this.chosen = true
     }
+    if (this.idle == true){
+        this.playIdle();
+    } else {
+        this.playWave();
+    }
+}
 
     animate(){
         this.index += this.speed;
     }
+
 }
