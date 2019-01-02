6d47aa7e9181c396800c39b1487e4503a91b42ab
jamiejamiebobamie <jmccrory@vt.edu>
2019-01-02 17:47:49 -0500

    commented-out modal close button and changed the opacity of the moal window

--- about_modal.js
+++ about_modal.js
@@ -7,7 +7,7 @@ var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -15,9 +15,9 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
